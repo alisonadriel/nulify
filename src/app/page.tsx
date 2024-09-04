9135c3acf404a9b086a960de246cf6daa2c64f97
Alison Adriel <adriel.dias15@gmail.com>
2024-09-03 22:14:23 -0300

    style: aside-bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import {Home as HomeScreen, Search, Disc} from 'lucide-react'
+import {Home as HomeScreen, Search, Disc, Library} from 'lucide-react'
 import Image from 'next/image';
+
 
 export default function Home() {
   return (
@@ -7,7 +8,7 @@ export default function Home() {
         <div className="flex items flex-1">
         <aside className="w-72 bg-zinc-950 p-6">
           <div className='flex items-center mb-6' >
-          <Image src="/nulify-logo.png" width={45} height={40} alt="nulify-icon-logo"/>
+          <Image src="/nulify-logo.png" width={40} height={30} alt="nulify-icon-logo"/>
           </div>
           <nav className="space-y-5">
             <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200 hover:text-zinc-100">
@@ -24,7 +25,17 @@ export default function Home() {
             </a>
           </nav>
 
-          <nav className='mt-10 pt-10 border-t border-zinc-600'></nav>
+          <nav className='mt-10 pt-10  flex flex-col gap-2'>
+             <div className='flex items-center gap-2'>
+             <h1 className=' font-semibold flex gap-1 items-center text-[16px] text-zinc-300 mt-3'>
+               <Library/> Playlists </h1>
+             </div>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100' >Self-Tilted</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Blurryface</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Trench</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Scaled and Icy</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Clancy</a>
+          </nav>
         </aside>
         <main className="flex-1 p-6"></main>
         </div>
