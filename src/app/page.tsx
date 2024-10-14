ca0c4fdfb29f97982218609b3620cdde7389f98c
Alison Adriel <adriel.dias15@gmail.com>
2024-10-14 20:11:50 -0300

    style: audiolines

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import {Home as HomeScreen, Search, Disc, Library, ChevronLeft, ChevronRight} from 'lucide-react'
+import {Home as HomeScreen, AudioLines, Play, Search, Disc, Library, ChevronLeft, ChevronRight} from 'lucide-react'
 import Image from 'next/image';
 
 
@@ -11,7 +11,7 @@ export default function Home() {
           <Image src="/nulify-logo.png" width={40} height={30} alt="nulify-icon-logo"/>
           </div>
           <nav className="space-y-5">
-            <a href="" className="flex items-center gap-3 text-sm font-light text-zinc-200 hover:text-zinc-100">
+            <a href="" className=" flex items-center gap-3 text-sm font-light text-zinc hover:text-zinc-100">
               <HomeScreen/>
               Home
             </a>
@@ -51,29 +51,47 @@ export default function Home() {
         <h1 className='font-extralight text-3xl mt-5 mb-4'>Home</h1>
 
         <div className='grid grid-cols-3 gap-4'>
-          <a href='https://open.spotify.com/album/30H0cGghxhBJ93v4h28XEt?si=IQqkahZ3S6CwGGiICpprsg' target='blank' className='bg-white/10 rounded font-normal flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+          <a href='https://open.spotify.com/album/30H0cGghxhBJ93v4h28XEt?si=IQqkahZ3S6CwGGiICpprsg' target='blank' className='bg-white/10 group rounded font-normal flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
           <Image src="/tkl.jpg" width={104} height={104} alt='Capa do álbum THE FRIST TIME do cantor The Kid Laroi' ></Image>
           <strong>THE FIRST TIME</strong>
+          <button className='p-2 text-yellow-nulify ml-auto mr-8 invisible group-hover:visible'>
+              <AudioLines/>
+            </button>
           </a>
-          <a href='https://open.spotify.com/album/30H0cGghxhBJ93v4h28XEt?si=IQqkahZ3S6CwGGiICpprsg' target='blank' className='bg-white/10 rounded font-normal flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+          <a href='https://open.spotify.com/album/30H0cGghxhBJ93v4h28XEt?si=IQqkahZ3S6CwGGiICpprsg' target='blank' className='bg-white/10 group rounded font-normal flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
           <Image src="/tkl.jpg" width={104} height={104} alt='Capa do álbum THE FRIST TIME do cantor The Kid Laroi' ></Image>
           <strong>THE FIRST TIME</strong>
+            <button className='p-2 text-yellow-nulify ml-auto mr-8 invisible group-hover:visible'>
+              <AudioLines/>
+            </button>
           </a>
-          <a href='https://open.spotify.com/album/30H0cGghxhBJ93v4h28XEt?si=IQqkahZ3S6CwGGiICpprsg' target='blank' className='bg-white/10 rounded font-normal flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+          <a href='https://open.spotify.com/album/30H0cGghxhBJ93v4h28XEt?si=IQqkahZ3S6CwGGiICpprsg' target='blank' className='bg-white/10 group rounded font-normal flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
           <Image src="/tkl.jpg" width={104} height={104} alt='Capa do álbum THE FRIST TIME do cantor The Kid Laroi' ></Image>
           <strong>THE FIRST TIME</strong>
+          <button className='p-2 text-yellow-nulify ml-auto mr-8 invisible group-hover:visible'>
+              <AudioLines/>
+            </button>
           </a>
-          <a href='https://open.spotify.com/album/30H0cGghxhBJ93v4h28XEt?si=IQqkahZ3S6CwGGiICpprsg' target='blank' className='bg-white/10 rounded font-normal flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+          <a href='https://open.spotify.com/album/30H0cGghxhBJ93v4h28XEt?si=IQqkahZ3S6CwGGiICpprsg' target='blank' className='bg-white/10 group rounded font-normal flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
           <Image src="/tkl.jpg" width={104} height={104} alt='Capa do álbum THE FRIST TIME do cantor The Kid Laroi' ></Image>
           <strong>THE FIRST TIME</strong>
+          <button className='p-2 text-yellow-nulify ml-auto mr-8 invisible group-hover:visible'>
+              <AudioLines/>
+            </button>
           </a>
-          <a href='https://open.spotify.com/album/30H0cGghxhBJ93v4h28XEt?si=IQqkahZ3S6CwGGiICpprsg' target='blank' className='bg-white/10 rounded font-normal flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+          <a href='https://open.spotify.com/album/30H0cGghxhBJ93v4h28XEt?si=IQqkahZ3S6CwGGiICpprsg' target='blank' className='bg-white/10 group rounded font-normal flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
           <Image src="/tkl.jpg" width={104} height={104} alt='Capa do álbum THE FRIST TIME do cantor The Kid Laroi' ></Image>
           <strong>THE FIRST TIME</strong>
+          <button className='p-2 text-yellow-nulify ml-auto mr-8 invisible group-hover:visible'>
+              <AudioLines/>
+            </button>
           </a>
-          <a href='https://open.spotify.com/album/30H0cGghxhBJ93v4h28XEt?si=IQqkahZ3S6CwGGiICpprsg' target='blank' className='bg-white/10 rounded font-normal flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+          <a href='https://open.spotify.com/album/30H0cGghxhBJ93v4h28XEt?si=IQqkahZ3S6CwGGiICpprsg' target='blank' className='bg-white/10 group rounded font-normal flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
           <Image src="/tkl.jpg" width={104} height={104} alt='Capa do álbum THE FRIST TIME do cantor The Kid Laroi' ></Image>
           <strong>THE FIRST TIME</strong>
+          <button className='p-2 text-yellow-nulify ml-auto mr-8 invisible group-hover:visible'>
+              <AudioLines/>
+            </button>
           </a>
         </div>
         </main>
