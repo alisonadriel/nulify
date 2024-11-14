ca565611ecff593e46b76a1007d600a859e50321
Alison Adriel <adriel.dias15@gmail.com>
2024-11-13 22:23:43 -0300

    adding components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import {Home as HomeScreen, Play, AudioLines, Search, Disc, Library, ChevronLeft, ChevronRight, Shuffle, SkipBack, SkipForward, Repeat, Repeat2, MonitorSmartphone, Volume2} from 'lucide-react'
-import Image from 'next/image';
+import { ChevronLeft, ChevronRight } from 'lucide-react'
 import { SideBar } from '@/components/SideBar';
 import { Footer } from '@/components/Footer';
 import { Recomendation } from '@/components/Recomendation';
+import { HomeSongs } from '@/components/HomeSongs';
 
 
 export default function Home() {
@@ -19,72 +19,8 @@ export default function Home() {
             <ChevronRight/>
           </button>
         </div>
-        
         <h1 className='font-extralight text-3xl mt-5 mb-4'>Home</h1>
-
-        <div className='grid grid-cols-3 gap-4'>
-          <a href='https://open.spotify.com/album/30H0cGghxhBJ93v4h28XEt?si=IQqkahZ3S6CwGGiICpprsg' target='blank' className='bg-white/5 group rounded font-normal flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
-          <Image src="/tkl.jpg" width={104} height={104} alt='Capa do álbum THE FRIST TIME do cantor The Kid Laroi' ></Image>
-          <div className='flex flex-col'>
-            <strong>THE FIRST TIME</strong>
-            <span className='text-sm text-zinc-500'>The Kid LAROI</span>
-          </div>
-          <button className='p-2 text-yellow-nulify ml-auto mr-8 invisible group-hover:visible'>
-              <AudioLines/>
-            </button>
-          </a>
-          <a href='https://open.spotify.com/album/30H0cGghxhBJ93v4h28XEt?si=IQqkahZ3S6CwGGiICpprsg' target='blank' className='bg-white/5 group rounded font-normal flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
-          <Image src="/tkl.jpg" width={104} height={104} alt='Capa do álbum THE FRIST TIME do cantor The Kid Laroi' ></Image>
-          <div className='flex flex-col'>
-            <strong>THE FIRST TIME</strong>
-            <span className='text-sm text-zinc-500'>The Kid LAROI</span>
-          </div>
-            <button className='p-2 text-yellow-nulify ml-auto mr-8 invisible group-hover:visible'>
-              <AudioLines/>
-            </button>
-          </a>
-          <a href='https://open.spotify.com/album/30H0cGghxhBJ93v4h28XEt?si=IQqkahZ3S6CwGGiICpprsg' target='blank' className='bg-white/5 group rounded font-normal flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
-          <Image src="/tkl.jpg" width={104} height={104} alt='Capa do álbum THE FRIST TIME do cantor The Kid Laroi' ></Image>
-          <div className='flex flex-col'>
-            <strong>THE FIRST TIME</strong>
-            <span className='text-sm text-zinc-500'>The Kid LAROI</span>
-          </div>
-          <button className='p-2 text-yellow-nulify ml-auto mr-8 invisible group-hover:visible'>
-              <AudioLines/>
-            </button>
-          </a>
-          <a href='https://open.spotify.com/album/30H0cGghxhBJ93v4h28XEt?si=IQqkahZ3S6CwGGiICpprsg' target='blank' className='bg-white/5 group rounded font-normal flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
-          <Image src="/tkl.jpg" width={104} height={104} alt='Capa do álbum THE FRIST TIME do cantor The Kid Laroi' ></Image>
-          <div className='flex flex-col'>
-            <strong>THE FIRST TIME</strong>
-            <span className='text-sm text-zinc-500'>The Kid LAROI</span>
-          </div>
-          <button className='p-2 text-yellow-nulify ml-auto mr-8 invisible group-hover:visible'>
-              <AudioLines/>
-            </button>
-          </a>
-          <a href='https://open.spotify.com/album/30H0cGghxhBJ93v4h28XEt?si=IQqkahZ3S6CwGGiICpprsg' target='blank' className='bg-white/5 group rounded font-normal flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
-          <Image src="/tkl.jpg" width={104} height={104} alt='Capa do álbum THE FRIST TIME do cantor The Kid Laroi' ></Image>
-          <div className='flex flex-col'>
-            <strong>THE FIRST TIME</strong>
-            <span className='text-sm text-zinc-500'>The Kid LAROI</span>
-          </div>
-          <button className='p-2 text-yellow-nulify ml-auto mr-8 invisible group-hover:visible'>
-              <AudioLines/>
-            </button>
-          </a>
-          <a href='https://open.spotify.com/album/30H0cGghxhBJ93v4h28XEt?si=IQqkahZ3S6CwGGiICpprsg' target='blank' className='bg-white/5 group rounded font-normal flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
-          <Image src="/tkl.jpg" width={104} height={104} alt='Capa do álbum THE FRIST TIME do cantor The Kid Laroi' ></Image>
-          <div className='flex flex-col'>
-            <strong>THE FIRST TIME</strong>
-            <span className='text-sm text-zinc-500'>The Kid LAROI</span>
-          </div>
-          <button className='p-2 text-yellow-nulify ml-auto mr-8 invisible group-hover:visible'>
-              <AudioLines/>
-            </button>
-          </a>
-        </div>
-
+        <HomeSongs/>
         <h2 className='font-extralight text-2xl mt-5 mb-4'>You might like</h2>
         <Recomendation/>
         </main>
---
+++ src/components/HomeSongs.tsx
@@ -0,0 +1,69 @@
+import { AudioLines } from "lucide-react"
+import Image from "next/image"
+
+export function HomeSongs(){
+  return(
+    <div className='grid grid-cols-3 gap-4'>
+          <a href='https://open.spotify.com/album/30H0cGghxhBJ93v4h28XEt?si=IQqkahZ3S6CwGGiICpprsg' target='blank' className='bg-white/5 group rounded font-normal flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+          <Image src="/tkl.jpg" width={104} height={104} alt='Capa do álbum THE FRIST TIME do cantor The Kid Laroi' ></Image>
+          <div className='flex flex-col'>
+            <strong>THE FIRST TIME</strong>
+            <span className='text-sm text-zinc-500'>The Kid LAROI</span>
+          </div>
+          <button className='p-2 text-yellow-nulify ml-auto mr-8 invisible group-hover:visible'>
+              <AudioLines/>
+            </button>
+          </a>
+          <a href='https://open.spotify.com/album/30H0cGghxhBJ93v4h28XEt?si=IQqkahZ3S6CwGGiICpprsg' target='blank' className='bg-white/5 group rounded font-normal flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+          <Image src="/tkl.jpg" width={104} height={104} alt='Capa do álbum THE FRIST TIME do cantor The Kid Laroi' ></Image>
+          <div className='flex flex-col'>
+            <strong>THE FIRST TIME</strong>
+            <span className='text-sm text-zinc-500'>The Kid LAROI</span>
+          </div>
+            <button className='p-2 text-yellow-nulify ml-auto mr-8 invisible group-hover:visible'>
+              <AudioLines/>
+            </button>
+          </a>
+          <a href='https://open.spotify.com/album/30H0cGghxhBJ93v4h28XEt?si=IQqkahZ3S6CwGGiICpprsg' target='blank' className='bg-white/5 group rounded font-normal flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+          <Image src="/tkl.jpg" width={104} height={104} alt='Capa do álbum THE FRIST TIME do cantor The Kid Laroi' ></Image>
+          <div className='flex flex-col'>
+            <strong>THE FIRST TIME</strong>
+            <span className='text-sm text-zinc-500'>The Kid LAROI</span>
+          </div>
+          <button className='p-2 text-yellow-nulify ml-auto mr-8 invisible group-hover:visible'>
+              <AudioLines/>
+            </button>
+          </a>
+          <a href='https://open.spotify.com/album/30H0cGghxhBJ93v4h28XEt?si=IQqkahZ3S6CwGGiICpprsg' target='blank' className='bg-white/5 group rounded font-normal flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+          <Image src="/tkl.jpg" width={104} height={104} alt='Capa do álbum THE FRIST TIME do cantor The Kid Laroi' ></Image>
+          <div className='flex flex-col'>
+            <strong>THE FIRST TIME</strong>
+            <span className='text-sm text-zinc-500'>The Kid LAROI</span>
+          </div>
+          <button className='p-2 text-yellow-nulify ml-auto mr-8 invisible group-hover:visible'>
+              <AudioLines/>
+            </button>
+          </a>
+          <a href='https://open.spotify.com/album/30H0cGghxhBJ93v4h28XEt?si=IQqkahZ3S6CwGGiICpprsg' target='blank' className='bg-white/5 group rounded font-normal flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+          <Image src="/tkl.jpg" width={104} height={104} alt='Capa do álbum THE FRIST TIME do cantor The Kid Laroi' ></Image>
+          <div className='flex flex-col'>
+            <strong>THE FIRST TIME</strong>
+            <span className='text-sm text-zinc-500'>The Kid LAROI</span>
+          </div>
+          <button className='p-2 text-yellow-nulify ml-auto mr-8 invisible group-hover:visible'>
+              <AudioLines/>
+            </button>
+          </a>
+          <a href='https://open.spotify.com/album/30H0cGghxhBJ93v4h28XEt?si=IQqkahZ3S6CwGGiICpprsg' target='blank' className='bg-white/5 group rounded font-normal flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+          <Image src="/tkl.jpg" width={104} height={104} alt='Capa do álbum THE FRIST TIME do cantor The Kid Laroi' ></Image>
+          <div className='flex flex-col'>
+            <strong>THE FIRST TIME</strong>
+            <span className='text-sm text-zinc-500'>The Kid LAROI</span>
+          </div>
+          <button className='p-2 text-yellow-nulify ml-auto mr-8 invisible group-hover:visible'>
+              <AudioLines/>
+            </button>
+          </a>
+        </div>
+  )
+}
